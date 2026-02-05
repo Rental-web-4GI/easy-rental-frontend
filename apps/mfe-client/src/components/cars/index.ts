e8c3e4434658586c31f8dd3081103567ee0d18b8
jordan-vuide <whitetransaction@gmail.com>
2026-02-05 16:12:33 +0100

    merge all

--- apps/mfe-client/src/components/cars/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CarsHero } from './CarsHero';
-export { default as CarsFilterSidebar } from './CarsFilterSidebar';
-export { default as ApiVehicleCard } from './ApiVehicleCard';
-export { default as Pagination } from './Pagination';
--- apps/mfe-client/src/types/reservationType.ts
+++
@@ -1,6 +0,0 @@
-export interface Reservation {
-  id: number;
-  issueDate: string;
-  status: string;
-  price: number;
-}
